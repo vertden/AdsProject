4a940f3edc11606291773c434bdc77749cbe9ff8
Denis Petrovsky <vertuss111@gmail.com>
2019-11-29 11:59:11 +0300

    create devise views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_203002) do
+ActiveRecord::Schema.define(version: 2019_11_29_084349) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2019_11_28_203002) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
